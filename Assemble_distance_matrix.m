705fa019d75559cc9b14ca0b81e6b14009fd5bf5
Michael Bode <mbode.web@gmail.com>
2021-09-05 16:04:18 +1000

    Add files via upload

---
+++ Assemble_distance_matrix.m
@@ -0,0 +1,68 @@
+clear all
+
+load SDM_1 SDM_1
+load SDM_2 SDM_2
+load SDM_3 SDM_3
+load SDM_4 SDM_4
+load SDM_5 SDM_5
+load SDM_6 SDM_6
+load SDM_7 SDM_7
+load SDM_8 SDM_8
+load SDM_9 SDM_9
+load SDM_10 SDM_10
+load SDM_11 SDM_11
+load SDM_12 SDM_12
+load SDM_13 SDM_13
+load SDM_14 SDM_14
+load SDM_15 SDM_15
+
+NDistanceMatrix = [SDM_1 ...
+                  SDM_2 ...
+                  SDM_3 ...
+                  SDM_4 ...
+                  SDM_5 ...
+                  SDM_6 ...
+                  SDM_7 ...
+                  SDM_8 ...
+                  SDM_9 ...
+                  SDM_10 ...
+                  SDM_11 ...
+                  SDM_12 ...
+                  SDM_13 ...
+                  SDM_14 ...
+                  SDM_15];
+% 
+% 
+% 
+% SDM_1 = DistanceMatrix(:,0001:1000);
+% SDM_2 = DistanceMatrix(:,1001:2000);
+% SDM_3 = DistanceMatrix(:,2001:3000);
+% SDM_4 = DistanceMatrix(:,3001:4000);
+% SDM_5 = DistanceMatrix(:,4001:5000);
+% SDM_6 = DistanceMatrix(:,5001:6000);
+% SDM_7 = DistanceMatrix(:,6001:7000);
+% SDM_8 = DistanceMatrix(:,7001:8000);
+% SDM_9 = DistanceMatrix(:,8001:9000);
+% SDM_10 = DistanceMatrix(:,9001:10000);
+% SDM_11 = DistanceMatrix(:,10001:11000);
+% SDM_12 = DistanceMatrix(:,11001:12000);
+% SDM_13 = DistanceMatrix(:,12001:13000);
+% SDM_14 = DistanceMatrix(:,13001:14000);
+% SDM_15 = DistanceMatrix(:,14001:15524);
+% 
+% 
+% save SDM_1 SDM_1
+% save SDM_2 SDM_2
+% save SDM_3 SDM_3
+% save SDM_4 SDM_4
+% save SDM_5 SDM_5
+% save SDM_6 SDM_6
+% save SDM_7 SDM_7
+% save SDM_8 SDM_8
+% save SDM_9 SDM_9
+% save SDM_10 SDM_10
+% save SDM_11 SDM_11
+% save SDM_12 SDM_12
+% save SDM_13 SDM_13
+% save SDM_14 SDM_14
+% save SDM_15 SDM_15
